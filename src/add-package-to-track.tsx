713b04c46c5a403d2ce34d3c67be43f9aee558d7
halprin <halprin@users.noreply.github.com>
2024-11-17 19:12:19 -0700

    Fill out form to track a new delivery

--- src/add-package-to-track.tsx
+++ src/add-package-to-track.tsx
@@ -1,40 +1,49 @@
-import { Form, ActionPanel, Action, showToast } from "@raycast/api";
+import { Form, ActionPanel, Action, showToast, Toast } from "@raycast/api";
+import providers from "./providers";
+import { FormValidation, useForm } from "@raycast/utils";
 
-type Values = {
-  textfield: string;
-  textarea: string;
-  datepicker: Date;
-  checkbox: boolean;
-  dropdown: string;
-  tokeneditor: string[];
-};
+interface AddTrackingForm {
+  name: string,
+  carrier: string,
+  trackingNumber: string,
+}
 
 export default function AddCommand() {
-  function handleSubmit(values: Values) {
-    console.log(values);
-    showToast({ title: "Submitted form", message: "See logs for submitted values" });
-  }
+
+  const { handleSubmit, itemProps } = useForm<AddTrackingForm>({
+    onSubmit(trackingForm) {
+
+      showToast({
+        style: Toast.Style.Success,
+        title: "New Delivery Added",
+        message: trackingForm.name,
+      });
+
+    },
+    validation: {
+      name: FormValidation.Required,
+      carrier: FormValidation.Required,
+      trackingNumber: FormValidation.Required,
+    },
+  });
+
 
   return (
     <Form
       actions={
         <ActionPanel>
-          <Action.SubmitForm onSubmit={handleSubmit} />
+          <Action.SubmitForm onSubmit={ handleSubmit } />
         </ActionPanel>
       }
     >
-      <Form.Description text="This form showcases all available form elements." />
-      <Form.TextField id="textfield" title="Text field" placeholder="Enter text" defaultValue="Raycast" />
-      <Form.TextArea id="textarea" title="Text area" placeholder="Enter multi-line text" />
-      <Form.Separator />
-      <Form.DatePicker id="datepicker" title="Date picker" />
-      <Form.Checkbox id="checkbox" title="Checkbox" label="Checkbox Label" storeValue />
-      <Form.Dropdown id="dropdown" title="Dropdown">
-        <Form.Dropdown.Item value="dropdown-item" title="Dropdown Item" />
+      <Form.Description text="Fill in the details of the delivery you want to track." />
+      <Form.TextField title="Name" placeholder="Name for the delivery" {...itemProps.name} />
+      <Form.Dropdown title="Carrier"  {...itemProps.carrier}>
+        {
+          Array.from(providers.values()).map(provider => (<Form.Dropdown.Item key={ provider.name } value={ provider.name } title={ provider.name } />))
+        }
       </Form.Dropdown>
-      <Form.TagPicker id="tokeneditor" title="Tag picker">
-        <Form.TagPicker.Item value="tagpicker-item" title="Tag Picker Item" />
-      </Form.TagPicker>
+      <Form.TextField title="Tracking number" placeholder="Tracking number from the carrier" {...itemProps.trackingNumber} />
     </Form>
   );
 }
